a3b99f66b3f0b8c19cd22ee86be49b107806ab77
unvalley <38400669+unvalley@users.noreply.github.com>
2026-02-26 16:31:24 +0000

    chore: bump rt to v0.1.9 (#2)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Formula/rt.rb
+++ Formula/rt.rb
@@ -1,13 +1,13 @@
 class Rt < Formula
   desc "rt is a CLI to run tasks interactively across different task runners"
   homepage "https://github.com/unvalley/rt"
-  version "0.1.8"
+  version "0.1.9"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/unvalley/rt/releases/download/v0.1.8/rt-cli-aarch64-apple-darwin-v0.1.8.tgz"
-      sha256 "d68438241eb14fc0f97f13b53dbad99ff2a4a5e7c6250ad63e3012dd3e1f5050"
+      url "https://github.com/unvalley/rt/releases/download/v0.1.9/rt-cli-aarch64-apple-darwin-v0.1.9.tgz"
+      sha256 "5fae30c6de48524c9b9597c252234323e2331daed7f36f64fc2b976771bdd515"
     end
   end
 
